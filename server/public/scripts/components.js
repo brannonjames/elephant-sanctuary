dcea066bd48243ce0af850527699a415f7e663c2
Jimmy Brannon <user.email>
2018-07-19 17:06:04 -0500

    add components and navbar

--- server/public/scripts/components.js
+++ server/public/scripts/components.js
@@ -1,5 +1,15 @@
 app
-  .component('allElephants', {
-    template: '<h1>All Elephants {{ vm.title }}</h2>',
-    controller: 'ElephantsController as vm'
-  });
+.component('allElephants', {
+  templateUrl: 'views/elephants.html',
+  controller: 'ElephantsController as vm'
+})
+.component('sanctuary', {
+  templateUrl: 'views/sanctuary.html',
+  controller: 'SanctuaryController as vm'
+})
+.component('humans', {
+  templateUrl: 'views/humans.html',
+  controller: 'HumanController as vm'
+})
+.component('jbHeader', { templateUrl: 'views/templates/header.html' })
+.component('navBar', { templateUrl: 'views/templates/nav.html' })
