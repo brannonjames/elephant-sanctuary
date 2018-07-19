809b38b6e78fef307c89a9388d65506bcab4b741
Jimmy Brannon <user.email>
2018-07-19 10:47:49 -0500

    created server file

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('server/public'));
+app.use(bodyParser.json());
+app.use('/humans', humanRoutes);
+app.use('/elephants', elephantRoutes);
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ message: err.message || 'An Error Occured' })
+});
+
+app.listen(PORT, () => {
+  console.log('elephant sanctuary running on port ', PORT);
+});
